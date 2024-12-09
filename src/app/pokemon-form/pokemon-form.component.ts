0832d2fa1ebd9b5de392b93849d302fe9f36aac7
Juan de Dios Barajas <juandebarco@MacBook-Pro-de-Juan.local>
2024-12-09 13:47:16 -0600

    Se hace la petición a pokemon api

---
+++ src/app/pokemon-form/pokemon-form.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-pokemon-form',
+  imports: [FormsModule],
+  templateUrl: './pokemon-form.component.html',
+  styleUrl: './pokemon-form.component.css'
+})
+export class PokemonFormComponent {
+  pokemonName: string = '';
+  pokemonData: any = null;
+
+  constructor(private http: HttpClient) {}
+
+  searchPokemon() {
+    console.log('Searching for: ' + this.pokemonName);
+
+    this.http.get('https://pokeapi.co/api/v2/pokemon/' + this.pokemonName).subscribe(data => {
+      this.pokemonData = data;
+      console.log('Pokemon data: ', this.pokemonData);
+    });
+  }
+}
