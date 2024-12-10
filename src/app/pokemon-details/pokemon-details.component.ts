e24729e0f3dc171b58fe0e2c0e1e89c53376e523
Juan de Dios Barajas <juandebarco@MacBook-Pro-de-Juan.local>
2024-12-10 12:56:21 -0600

    Enhance Pokémon details display with ability information and effects; implement API calls for abilities

--- src/app/pokemon-details/pokemon-details.component.ts
+++ src/app/pokemon-details/pokemon-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
 import { PokemonTypeComponent } from '../pokemon-type/pokemon-type.component';
@@ -11,4 +12,53 @@ import { PokemonTypeComponent } from '../pokemon-type/pokemon-type.component';
 })
 export class PokemonDetailsComponent {
   @Input() pokemon: any = null;
+
+  abilityDetails: any = null;
+  abilityEffect: string = '';
+  hiddenAbilityDetails: any = null;
+  hiddenAbilityEffect: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['pokemon'] && this.pokemon) {
+      this.getAbilityInfo(this.pokemon.abilities[0].ability.url);
+      if (this.pokemon.abilities.length > 1) {
+        this.getHiddenAbilityInfo(this.pokemon.abilities[1].ability.url);
+      } else {
+        this.hiddenAbilityDetails = null;
+        this.hiddenAbilityEffect = 'No hay información disponible.';
+      }
+    }
+  }
+
+  getAbilityInfo(url: string) : any {
+    this.abilityDetails = null;
+    this.abilityEffect = '';
+    this.http.get(url).subscribe(data => {
+      console.log('Ability data:', data);
+      this.abilityDetails = data;
+
+      if (this.abilityDetails.effect_entries.length > 0) {
+        this.abilityEffect = this.abilityDetails.effect_entries.find((entry: any) => entry.language.name === 'en').effect;
+      } else {
+        this.abilityEffect = 'No hay información disponible.';
+      }
+    });
+  }
+
+  getHiddenAbilityInfo(url: string) : any {
+    this.hiddenAbilityDetails = null;
+    this.hiddenAbilityEffect = '';
+    this.http.get(url).subscribe(data => {
+      console.log('Hidden ability data:', data);
+      this.hiddenAbilityDetails = data;
+
+      if (this.hiddenAbilityDetails.effect_entries.length > 0) {
+        this.hiddenAbilityEffect = this.hiddenAbilityDetails.effect_entries.find((entry: any) => entry.language.name === 'en').effect;
+      } else {
+        this.hiddenAbilityEffect = 'No hay información disponible.';
+      }
+    });
+  }
 }
